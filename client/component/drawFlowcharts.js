5402b746588a1f154b188cd7c0acd3cfd27c7901
绝云 <wensen.lws@alibaba-inc.com>
2015-07-29 21:47:52 +0800

    remove styles when drawing flowcharts

--- client/component/drawFlowcharts.js
+++ client/component/drawFlowcharts.js
@@ -17,62 +17,18 @@ define([
     var drawOptions = {
         'x': 0,
         'y': 0,
-        'line-width': 3,
-        'line-length': 50,
+        'line-width': 2,
+        'line-length': 40,
         'text-margin': 10,
         'font-size': 14,
         'font-color': 'black',
-        'line-color': 'black',
-        'element-color': 'black',
-        'fill': 'white',
+        'line-color': 'grey',
+        'element-color': 'grey',
+        'fill': 'lightyellow',
         'yes-text': 'yes',
         'no-text': 'no',
         'arrow-end': 'block',
         'scale': 1,
-        // style symbol types
-        'symbols': {
-            'start': {
-                'font-color': 'red',
-                'element-color': 'green',
-                'fill': 'yellow'
-            },
-            'end':{
-                'class': 'end-element'
-            }
-        },
-        // even flowstate support ;-)
-        'flowstate': {
-            'past': {
-                'fill': '#CCCCCC',
-                'font-size': 12
-            },
-            'current': {
-                'fill': 'yellow',
-                'font-color': 'red',
-                'font-weight': 'bold'
-            },
-            'future': {
-                'fill': '#FFFF99'
-            },
-            'request': {
-                'fill': 'blue'
-            },
-            'invalid': {
-                'fill': '#444444'
-            },
-            'approved': {
-                'fill': '#58C4A3',
-                'font-size': 12,
-                'yes-text': 'APPROVED',
-                'no-text': 'n/a'
-            },
-            'rejected': {
-                'fill': '#C45879',
-                'font-size': 12,
-                'yes-text': 'n/a',
-                'no-text': 'REJECTED'
-            }
-        }
     };
     return function(scope) {
         each(domQuery.all('.flowchart', scope), function(container) {
