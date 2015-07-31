b4622de4d94000e98455c0533ad02c3196bf1e03
绝云 <wensen.lws@alibaba-inc.com>
2015-07-31 16:34:40 +0800

    markdown previewer & editor enhancemant

--- client/markdownPreviewer/main.js
+++ client/markdownPreviewer/main.js
@@ -5,23 +5,34 @@ define([
     'pastry/pastry',
     'pastry/dom/query',
     '../component/drawFlowcharts',
-    '../component/marked'
+    '../component/marked',
+    '../component/Resizer'
 ], function(
     pastry,
     domQuery,
     drawFlowcharts,
-    marked
+    marked,
+    Resizer
 ) {
     'use strict';
     /*
      * @author      : 绝云（wensen.lws）
      * @description : description
      */
-    var previewerDomNode = domQuery.one('section#markdown-previewer'),
+    var container = domQuery.one('article.markdown-container'),
+        previewerDomNode = domQuery.one('section#markdown-previewer'),
         content = domQuery.one('script#markdown-content').innerHTML;
 
     previewerDomNode.innerHTML = marked(content);
     mermaid.init(); // render graphs
     drawFlowcharts(previewerDomNode);
+
+    new Resizer(container, {
+        directions: ['e', 'w'],
+        minWidth: 400,
+        maxWidth: 1400,
+        onResize: function() {
+        }
+    });
 });
 
