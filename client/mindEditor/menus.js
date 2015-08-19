2c968010650b34ac6959c4a10340391dcd78a95f
绝云 <wensen.lws@alibaba-inc.com>
2015-08-19 17:22:13 +0800

    first step for mind editor!

---
+++ client/mindEditor/menus.js
@@ -0,0 +1,19 @@
+/* jshint strict: true, undef: true, unused: true */
+/* global define */
+
+define([
+    //'pastry/pastry'
+], function(
+    //pastry
+) {
+    'use strict';
+    /*
+     * @author: 绝云（wensen.lws）
+     * @description: description
+     */
+    return {
+        init: function() {
+        }
+    };
+});
+
---
+++ client/mindEditor/store.js
@@ -0,0 +1,16 @@
+/* jshint strict: true, undef: true, unused: true */
+/* global define */
+
+define([
+    '../component/Cache'
+], function(
+    Cache
+) {
+    'use strict';
+    /*
+     * @author: 绝云（wensen.lws）
+     * @description: description
+     */
+    return new Cache();
+});
+
