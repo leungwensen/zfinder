2a141a27392b2a7cdc5a0c88b230c089142aedb9
绝云 <wensen.lws@alibaba-inc.com>
2015-08-25 15:52:01 +0800

    optimize building

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,37 @@ module.exports = function (grunt) {
                     'client/stylesheet/file-markdown.css': 'client/stylesheet/file-markdown.less',
                     'client/stylesheet/app-markdownEditor.css': 'client/stylesheet/app-markdownEditor.less',
                     'client/stylesheet/app-mindEditor.css': 'client/stylesheet/app-mindEditor.less',
+                },
+                js: {
+                    'markdownPreviewer': {
+                        baseUrl: 'client/markdownPreviewer',
+                        src: 'client/markdownPreviewer/main.js',
+                        out: 'client/markdownPreviewer/main.all.js',
+                    }
+                },
+                requirejs: {
+                    paths: {
+                        'pastry': 'empty:',
+                    }
+                },
+                concat: {
+                    markdownPreviewer: {
+                        src: [
+                            'lib/leungwensen/forked/marked.min.js',
+                            'lib/Khan/KaTeX/katex.min.js',
+                            'lib/knsv/mermaid/dist/mermaid.min.js',
+                            'lib/DmitryBaranovskiy/raphael/raphael-min.js',
+                            'lib/adrai/flowchart.js/release/flowchart-1.4.0.js',
+                        ],
+                        dest: 'client/markdownPreviewer/libs.min.js'
+                    }
+                },
+                uglify: {
+                    markdownPreviewer: {
+                        files: {
+                            'client/markdownPreviewer/main.min.js': 'client/markdownPreviewer/main.all.js',
+                        }
+                    }
                 }
             }
         }
