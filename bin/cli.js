ff62dc6f47a6435fbffe400073006349c7074063
leungwensen <leungwensen@gmail.com>
2015-10-16 11:56:57 +0800

    reconstruct zfinder-cli/bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,34 @@
 var commander = require('commander');
 var inquirer = require('inquirer');
 var sprintf = require('zero-fmt-sprintf');
+var pkg = require('../package.json');
+
+commander
+    .version(pkg.version);
+
+// start a new zfinder server
+commander
+    .command('serve [root]')
+    .description('start a zfinder server')
+    .option('-o, --open', 'open on start')
+    .option('-p, --port <port>', 'specify the port which server will run on')
+    .option('-c, --config <config>', 'specify the configuration file')
+    .option('-l, --localmiddleware [middleware...]', 'specify paths to local middlewares')
+    .action(function(root, options) {
+        require('../lib/serve')(
+            require('../lib/config-server')(root, options)
+        );
+    });
+
+// build a path
+commander
+    .command('build [root]')
+    .description('run zfinder building task')
+    .option('-c, --config <config>', 'specify the configuration file')
+    .option('-r, --recursively', 'build all files recursively')
+    .action(function(root, options) {
+        require('../lib/build')(root, options);
+    });
 
 // creating a new middleware for zfinder
 commander
@@ -40,29 +68,26 @@ commander
         require('../lib/killall.js')();
     });
 
-// start a new zfinder server
-commander
-    .command('serve [root]')
-    .description('start a zfinder server')
-    .option('-o, --open', 'open on start')
-    .option('-p, --port <port>', 'specify the port which server will run on')
-    .option('-c, --config <config>', 'specify the configuration file')
-    .option('-l, --localmiddleware [middleware...]', 'specify paths to local middlewares')
-    .action(function(root, options) {
-        require('../lib/serve')(
-            require('../lib/config-server')(root, options)
-        );
-    });
-
-// build a path
-commander
-    .command('build [root]')
-    .description('start a zfinder server')
-    .option('-c, --config <config>', 'specify the configuration file')
-    .option('-r, --recursively', 'build all files recursively')
-    .action(function(root, options) {
-        require('../lib/build')(root, options);
-    });
+commander.on('--help', function(){
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ zfinder-cli serve --open --port 9090 --config ./config.yaml --localmiddleware ~/path/to/local/middlewares');
+    console.log('    $ zfinder-cli serve -o -p 9090 -c ./config.yaml -l ~/path/to/local/middlewares');
+    console.log('    $ zfinder-cli build --recursively --config ./config.yaml');
+    console.log('    $ zfinder-cli build --r -c ./config.yaml');
+    console.log('    $ zfinder-cli create-middleware sample-mwname --root ~/repo/');
+    console.log('    $ zfinder-cli create-middleware sample-mwname --r ~/repo/');
+    console.log('    $ zfinder-cli killall');
+    console.log('    $ zfinder-cli --help');
+    console.log('    $ zfinder-cli -h');
+    console.log('    $ zfinder-cli --version');
+    console.log('    $ zfinder-cli -V');
+    console.log('');
+});
 
 commander.parse(process.argv);
+
+if (process.argv.length === 2) {
+    commander.outputHelp();
+}
 
