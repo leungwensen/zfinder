4ff278a407cfd272355e2314895d49f0823265b7
leungwensen <leungwensen@gmail.com>
2015-10-16 16:18:01 +0800

    add more template code

--- templates/lib/index.js
+++ templates/lib/index.js
@@ -1,5 +1,31 @@
 /* jshint node: true, loopfunc: true, undef: true, unused: true */
 ///* global */
 
-module.exports = {};
+var path = require('path');
+var Middleware = require('zfinder-middleware');
+var Route = require('zfinder-route');
+var objectUtils = require('zero-lang-object');
+var zfinderUtils = require('zfinder-utils');
+
+var defaultConfig = zfinderUtils.loadYamlConfig(path.resolve(__dirname, '../config.yaml'));
+
+var mw = new Middleware()
+    .addRoute(new Route({
+        // properties
+        name: '',
+        method: 'get',
+        url: '*',
+        priority: 1,
+
+        // methods
+        serve: function(options) {
+            options = objectUtils.extend({}, defaultConfig, options);
+            // your code goes here
+            return function(req, res, next) {
+                next();
+            };
+        },
+    }));
+
+module.exports = mw;
 
