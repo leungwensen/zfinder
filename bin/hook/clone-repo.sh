bfca3fa3cb9d8859eb140d3d2f7e0fe6bfde6737
leungwensen <leungwensen@gmail.com>
2016-07-20 14:24:57 +0800

    remove remaining trash

--- bin/hook/clone-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#usage:
-#    ./clone-repo.sh $url-of-repo $path-of-repo
-
-#git config user.name "wensen.lws"
-git clone $1 $2
-
--- bin/hook/update-repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#usage:
-#    ./update-repo.sh $path-of-repo
-
-cd $1
-#git config user.name "wensen.lws" #example
-git stash #stash local changes!
-git pull
-
