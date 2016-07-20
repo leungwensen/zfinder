d7219467ffacf06c088e85244b525c4ed7343b2f
leungwensen <leungwensen@gmail.com>
2016-07-20 16:04:10 +0800

    read-file, write-file handlers

--- spec/api.sh
+++ spec/api.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-curl -u zfinder:awesome -X POST                              -L "http://localhost:9090/_temp/temp.md?_handler=make-dir"
-curl -u zfinder:awesome -X GET    -d "newPath=/temp/temp.md" -L "http://localhost:9090?_handler=read-dir"
-curl -u zfinder:awesome -X PUT    -d "newPath=test"          -L "http://localhost:9090?_handler=rename-path"
-curl -u zfinder:awesome -X PUT    -d "newPath=_temp/temp"    -L "http://localhost:9090/_temp/temp.md?_handler=rename-path"
-curl -u zfinder:awesome -X DELETE                            -L "http://localhost:9090/_temp/temp?_handler=remove-path"
+printf "make-dir#####################################################################################################\n"
+curl -u zfinder:awesome -X POST -L "http://localhost:9090/_temp/temp.md?_handler=make-dir"
 
+printf "\nread-dir###################################################################################################\n"
+curl -u zfinder:awesome -X GET -d "newPath=/temp/temp.md" -L "http://localhost:9090?_handler=read-dir"
+
+printf "\nread-file##################################################################################################\n"
+curl -u zfinder:awesome -X GET -L "http://localhost:9090/README.md?_handler=read-file"
+
+printf "\nrename-path################################################################################################\n"
+curl -u zfinder:awesome -X PUT -d "newPath=test" -L "http://localhost:9090?_handler=rename-path"
+curl -u zfinder:awesome -X PUT -d "newPath=_temp/temp" -L "http://localhost:9090/_temp/temp.md?_handler=rename-path"
+
+printf "\nremove-path################################################################################################\n"
+curl -u zfinder:awesome -X DELETE -L "http://localhost:9090/_temp/temp?_handler=remove-path"
+
+printf "\write-file##################################################################################################\n"
+curl -u zfinder:awesome -X PUT -d "content=test" -L "http://localhost:9090/_temp/temp.txt?_handler=write-file"
+curl -u zfinder:awesome -X PUT -d "content=test" -L "http://localhost:9090/_temp/.temp.log?_handler=write-file"
+
+printf "\n"
