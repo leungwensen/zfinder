1598e636dc5d0e3ec46d93be8b7533dd4a9938cf
leungwensen <leungwensen@gmail.com>
2016-07-20 11:56:00 +0800

    rename handlers

--- spec/api.sh
+++ spec/api.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-# mkdir
-curl -u zfinder:awesome -X POST "http://localhost:9090/_temp/temp.md?_handler=mkdir"
+curl -u zfinder:awesome -X POST                              -L "http://localhost:9090/_temp/temp.md?_handler=make-dir"
+curl -u zfinder:awesome -X GET    -d "newPath=/temp/temp.md" -L "http://localhost:9090?_handler=read-dir"
+curl -u zfinder:awesome -X PUT    -d "newPath=test"          -L "http://localhost:9090?_handler=rename-path"
+curl -u zfinder:awesome -X PUT    -d "newPath=_temp/temp"    -L "http://localhost:9090/_temp/temp.md?_handler=rename-path"
+curl -u zfinder:awesome -X DELETE                            -L "http://localhost:9090/_temp/temp?_handler=remove-path"
 
-# readdir
-curl -u zfinder:awesome "http://localhost:9090?_handler=readdir&newPath=/temp/temp.md"
-
-# rename
-curl -u zfinder:awesome -X PUT -d "newPath=test" "http://localhost:9090?_handler=rename"
-curl -u zfinder:awesome -X PUT -d "newPath=_temp/temp" "http://localhost:9090/_temp/temp.md?_handler=rename"
-
-# rmdir
-curl -u zfinder:awesome -X DELETE "http://localhost:9090/_temp/temp?_handler=rmdir"
