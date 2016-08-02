3b3184a30bf68c1910aba0f291e4fe748b643f64
leungwensen <leungwensen@gmail.com>
2016-08-02 14:21:47 +0800

    template importing

--- gulp/template2module.js
+++ gulp/template2module.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const lang = require('zero-lang');
 const path = require('path');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const through = require('through2');
 const tpl2mod = require('template2module');
 const underscoreEngine = tpl2mod.engines.underscore;
-const lang = require('zero-lang');
 const config = require('./config');
 
 underscoreEngine.outerScopeVars.JSON = true;
+
+const REGEXP = {
+  importTag: /<import\s+src="\S*"><\/import>/g,
+  srcPath: /src="(\S*)"/,
+};
+function importing(content, resourcePath) {
+  const match = content.match(REGEXP.importTag);
+  if (match) {
+    lang.each(match, (m) => {
+      const sourcePath = m.match(REGEXP.srcPath)[1];
+      const absoluteSourcePath = path.resolve(path.dirname(resourcePath), sourcePath);
+      const sourceOriginContent = fs.readFileSync(absoluteSourcePath, 'utf8');
+      const sourceDistContent = importing(sourceOriginContent, absoluteSourcePath);
+      content = content.replace(m, sourceDistContent);
+    });
+  }
+  return content;
+}
 
 function renderTemplates() {
   return through.obj(function render(file, enc, cb) {
@@ -25,7 +44,8 @@ function renderTemplates() {
     try {
       gutil.log(file.path);
       // @TODO add svg sprite file as needed, instead of putting the whole evil-icons svg file
-      const content = underscoreEngine.render(file.contents.toString('utf8'), file.path, 'commonjs');
+      const templateContent = importing(file.contents.toString('utf8'), file.path);
+      const content = underscoreEngine.render(templateContent, file.path, 'commonjs');
       file.contents = new Buffer(`/* eslint-disable */ ${content}`);
     } catch (err) {
       this.emit('error', new gutil.PluginError('template2module', err.toString()));
@@ -51,4 +71,3 @@ lang.each(config.templateDirs, (dir) => {
 });
 
 gulp.task('template2module', lang.map(config.templateDirs, (dir) => `template2module-${dir}`));
-
