202eb450265390ffb5e2135095294892261a8ab7
leungwensen <leungwensen@gmail.com>
2016-09-18 17:43:03 +0800

    #18 cleaning up dependencies

--- gulp/commonjs2es6.js
+++ gulp/commonjs2es6.js
@@ -3,19 +3,24 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 const plumber = require('gulp-plumber');
 const through = require('through2');
-const Transformer = require('lebab').Transformer;
-const transformer = new Transformer({
-  // 'class': true,
-  // 'arg-spread': true,
-  arrow: true,
-  commonjs: true,
-  let: true,
-  template: true,
-  'default-param': true,
-  'no-strict': true,
-  'obj-method': true,
-  'obj-shorthand': true
-});
+const lebab = require('lebab');
+
+const transformOptions = [
+  'arg-spread',
+  'arrow',
+  'class',
+  'commonjs',
+  'default-param',
+  'exponent',
+  'for-of',
+  'includes',
+  'let',
+  // 'multi-var',
+  'no-strict',
+  'obj-method',
+  'obj-shorthand',
+  'template',
+];
 
 function toES6() {
   return through.obj(function render(file, enc, cb) {
@@ -30,8 +35,8 @@ function toES6() {
 
     try {
       gutil.log(file.path);
-      const content = transformer.run(file.contents.toString('utf8'));
-      file.contents = new Buffer(content);
+      const result = lebab.transform(file.contents.toString('utf8'), transformOptions);
+      file.contents = new Buffer(result.code);
     } catch (err) {
       this.emit('error', new gutil.PluginError('commonjs2es6', err.toString()));
     }
@@ -44,11 +49,11 @@ function toES6() {
 // 这个脚本不能多次执行，因此在临时文件夹内进行
 gulp.task(
   'commonjs2es6',
-  () => gulp.src(path.resolve(__dirname, '../lib/**/*.js')) // FIXME modify this to specify target
+  () => gulp.src(path.resolve(__dirname, '../spec/**/*.js')) // FIXME modify this to specify target
     .pipe(plumber())
     .pipe(toES6())
     .on('error', (err) => {
       gutil.log(gutil.colors.red(err.message));
     })
-    .pipe(gulp.dest(path.resolve(__dirname, '../_temp/'))) // FIXME modify this to specify target
+    .pipe(gulp.dest(path.resolve(__dirname, '../spec/'))) // FIXME modify this to specify target
 );
